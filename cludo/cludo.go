2daf762751dbd6c6a8e75ae5dbcd825eed5656e8
MoltenWolfCub <oliver@tomlins.org.uk>
2024-06-22 22:02:48 +0100

    started implementing questions

--- cludo/cludo.go
+++ cludo/cludo.go
@@ -1,5 +1,7 @@
 package cludo
 
+import "slices"
+
 type Option struct {
 	name  string
 	found bool
@@ -36,19 +38,49 @@ func (q QuestionCategory) HasKnownSolution() bool {
 	return len(available) == 1
 }
 
+type Answer int
+
+const (
+	WhoAnswer Answer = iota
+	WhatAnswer
+	WhereAnswer
+	NoAnswer
+)
+
+type Question struct {
+	whoPart   Option
+	whatPart  Option
+	wherePart Option
+
+	asker    Player
+	answerer Player
+
+	answer Answer
+}
+
+func NewQuestion(who, what, where Option, asker, answerer Player) Question {
+	return Question{
+		whoPart:   who,
+		whatPart:  what,
+		wherePart: where,
+		asker:     asker,
+		answerer:  answerer,
+	}
+}
+
 type Player string
 
 type Game struct {
-	who   QuestionCategory
-	what  QuestionCategory
-	where QuestionCategory
+	whoCategory   QuestionCategory
+	whatCategory  QuestionCategory
+	whereCategory QuestionCategory
 
-	otherPlayers []Player
+	players []Player
 }
 
 func NewDefaultGame(otherPlayers []string) Game {
 	g := Game{
-		who: NewQuestionCategory(
+		whoCategory: NewQuestionCategory(
 			NewOption("green"),
 			NewOption("mustard"),
 			NewOption("peacock"),
@@ -56,7 +88,7 @@ func NewDefaultGame(otherPlayers []string) Game {
 			NewOption("scarlet"),
 			NewOption("white"),
 		),
-		what: NewQuestionCategory(
+		whatCategory: NewQuestionCategory(
 			NewOption("wrench"),
 			NewOption("candlestick"),
 			NewOption("dagger"),
@@ -64,7 +96,7 @@ func NewDefaultGame(otherPlayers []string) Game {
 			NewOption("lead pipe"),
 			NewOption("rope"),
 		),
-		where: NewQuestionCategory(
+		whereCategory: NewQuestionCategory(
 			NewOption("bathroom"),
 			NewOption("study"),
 			NewOption("dining room"),
@@ -77,9 +109,27 @@ func NewDefaultGame(otherPlayers []string) Game {
 		),
 	}
 
-	for _, p := range otherPlayers {
-		g.otherPlayers = append(g.otherPlayers, Player(p))
+	for _, other := range otherPlayers {
+		p := Player(other)
+
+		if p == "THIS" {
+			panic("Can't have a player called `THIS`")
+		}
+		if slices.Contains(g.players, p) {
+			panic("Can't have 2 players with the same name")
+		}
+
+		g.players = append(g.players, p)
 	}
+	g.players = append(g.players, "THIS")
 
 	return g
+}
+
+func (g Game) EnsureValidQuestion(q Question) bool {
+	return false
+}
+
+func (g Game) DoTurn() {
+
 }
--- cludo/cludo_test.go
+++ cludo/cludo_test.go
@@ -50,3 +50,60 @@ func TestHasKnownSolutionWithoutOptions(t *testing.T) {
 		t.Error("Question.HasKnownSolution() Found a solution when there were no options left.")
 	}
 }
+
+func GenSampleGame() cludo.Game {
+	players := []string{
+		"alice",
+		"bob",
+		"charlie",
+	}
+	return cludo.NewDefaultGame(players)
+}
+
+func TestEnsureValidQuestionWithValid(t *testing.T) {
+	game := GenSampleGame()
+
+	question := cludo.NewQuestion(
+		cludo.NewOption("plum"),
+		cludo.NewOption("dagger"),
+		cludo.NewOption("study"),
+		cludo.Player("THIS"),
+		cludo.Player("alice"),
+	)
+
+	if !game.EnsureValidQuestion(question) {
+		t.Error("Game.EnsureValidQuestion() Question wasn't valid when it should've been")
+	}
+}
+
+func TestEnsureValidQuestionWithInvalidQuestionComponents(t *testing.T) {
+	game := GenSampleGame()
+
+	question := cludo.NewQuestion(
+		cludo.NewOption("eva smith"),
+		cludo.NewOption("bleach"),
+		cludo.NewOption("the factory"),
+		cludo.Player("THIS"),
+		cludo.Player("alice"),
+	)
+
+	if game.EnsureValidQuestion(question) {
+		t.Error("Game.EnsureValidQuestion() Question was deemed valid when it wasn't made of elements from the game")
+	}
+}
+
+func TestEnsureValidQuestionWithInvalidPlayers(t *testing.T) {
+	game := GenSampleGame()
+
+	question := cludo.NewQuestion(
+		cludo.NewOption("mustard"),
+		cludo.NewOption("rope"),
+		cludo.NewOption("kitchen"),
+		cludo.Player("eric"),
+		cludo.Player("inspector goole"),
+	)
+
+	if game.EnsureValidQuestion(question) {
+		t.Error("Game.EnsureValidQuestion() Question was deemed valid when the players weren't ones in the game")
+	}
+}
