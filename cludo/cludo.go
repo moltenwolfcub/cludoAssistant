2ddf8d79e56171a9c29793079629d9570fde2adb
MoltenWolfCub <oliver@tomlins.org.uk>
2024-06-22 20:17:24 +0100

    Implemented basic data structure

--- cludo/cludo.go
+++ cludo/cludo.go
@@ -1,1 +1,23 @@
 package cludo
+
+type Option struct {
+	name  string
+	found bool
+}
+
+func NewOption(name string) Option {
+	return Option{
+		name:  name,
+		found: false,
+	}
+}
+
+type Question struct {
+	options []Option
+}
+
+func NewQuestion(options ...Option) Question {
+	return Question{
+		options: options,
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/moltenwolfcub/cludoAssistant/cludo"
+)
+
+func main() {
+	who := cludo.NewQuestion(
+		cludo.NewOption("Green"),
+		cludo.NewOption("Mustard"),
+		cludo.NewOption("Peacock"),
+		cludo.NewOption("Plum"),
+		cludo.NewOption("Scarlet"),
+		cludo.NewOption("White"),
+	)
+	fmt.Println(who)
+}
